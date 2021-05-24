738aeecf58b47219ce390206537d682cc9feb4c0
rG <12301raghavgoyal@gmail.com>
2021-05-24 11:38:17 +0530

    Add Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface Props {}
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className="px-10 md:px-16 py-8">
+      <span className="text-2xl md:text-3xl font-bold tracking-wider text-_blue">
+        <span className="tracking-tighter font-extrabold">
+          ___&nbsp;
+        </span>
+        board
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;
