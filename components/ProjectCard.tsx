8019148accc3d8f745b2aa083f40b499fd36d09e
rG <12301raghavgoyal@gmail.com>
2021-05-31 11:02:53 +0530

    Change project card style

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Card from "./Card";
 import { ArrowRightIcon as ArrowRight } from "@heroicons/react/outline";
 
 interface Props {
@@ -9,15 +8,15 @@ interface Props {
 
 const ProjectCard = ({ heading, description }: Props) => {
   return (
-    <Card className="flex flex-row items-center space-x-5 group">
+    <div className="card flex flex-row items-center space-x-8 group">
       <div className="flex flex-col justify-evenly group-hover:text-gray-800 w-auto h-full">
         <h1 className="text-xl font-semibold">{heading}</h1>
         <span>{description}</span>
       </div>
-      <div className="h-full flex flex-col justify-end">
+      <div className="h-full flex flex-col justify-center">
         <ArrowRight className="w-5 h-5 text-white group-hover:text-_blue" />
       </div>
-    </Card>
+    </div>
   );
 };
 
